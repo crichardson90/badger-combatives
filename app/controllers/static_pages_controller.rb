97ad9c57145a4168ec813f64e6860590388a9743
Coty Richardson <cotyrichardson@Cotys-MacBook-Pro.local>
2018-12-04 20:37:41 -0600

    FEATURE: Added Facebook events API and Figaro

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,5 @@
 class StaticPagesController < ApplicationController
   def index
-    access_token "EAAJJ5uNd7RoBAIGt78sYrXGM8TvgxZCoLBAstKQPtqICU7S3pisZCZAMt01yZCXMllJm2waFZCAZAiK4c6nRRQZA6PrYO4DZADCYIaTqOb1xP7oWBJKzC2WZB3eNBq9eDGR9sgzadDgKa8zhJxkZAp70WfW753ex0gq2JYrKzey10dVgZDZD"
   end
 
   def home
