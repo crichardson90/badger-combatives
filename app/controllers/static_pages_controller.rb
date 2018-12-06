1457bb8f028f88dc90306d86f31a673ae253b39e
Coty Richardson <cotyrichardson@Cotys-MacBook-Pro.local>
2018-12-06 06:04:28 -0600

    FEATURE: Added gallery and merch bootstrap template

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,32 +1,3 @@
 class StaticPagesController < ApplicationController
-  def index
-  end
 
-  def home
-    
-  end
-
-  def about
-    
-  end
-
-  def classes
-    
-  end
-
-  def gallery
-    
-  end
-
-  def events
-    
-  end
-
-  def merch
-    
-  end
-
-  def contact
-    
-  end
 end
