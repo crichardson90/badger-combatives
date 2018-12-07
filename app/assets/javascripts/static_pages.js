3a9bebc3ff75fbf473628037fc1cb2c445ea82ad
Coty Richardson <cotyrichardson@Cotys-MacBook-Pro.local>
2018-12-06 21:20:14 -0600

    FEATURE: Added gallery/merch images and jquery modal to images

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,3 +1,19 @@
+// Get the modal
+$(document).ready(function() {
+  var modal = $('#myModal');
+  var modalImg = $('#modalImg');
+  var items = $('.item');
 
+  items.on("click", function() {
+    modal.fadeIn();
+    modalImg.attr("src", this.src);
+  });
 
+  window.onclick = function(event) {
+    var sameModal = document.getElementById("myModal");
 
+    if (event.target === sameModal) {
+      modal.fadeOut();
+    }
+  }
+})
