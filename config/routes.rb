6c770d3e79d501a1e8c33753e660469df91e78e1
Coty Richardson <cotyrichardson@Cotys-MacBook-Pro.local>
2018-12-10 20:22:54 -0600

    FEATURE: updated gallery, contact, and css

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,5 @@ Rails.application.routes.draw do
    get '/home' => 'static_pages#home'
    get '/about' => 'static_pages#about'
    get '/classes' => 'static_pages#classes'
-   get '/contact' => 'static_pages#contact'
-
-
 end
  
